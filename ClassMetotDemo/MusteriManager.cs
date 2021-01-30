1fb5c86faa59b586ec9a34fd3df01d0faa2bcf69
Barış Osmanoğlu <73604542+barisosmanoglu@users.noreply.github.com>
2021-01-30 22:52:57 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi, Müşteri bilgileri:\n "  +musteri.Id+ "\n" + musteri.Ad+ "\n"+ musteri.SoyAd);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi :\n " + musteri.Id + "\n" + musteri.Ad + "\n" + musteri.SoyAd );
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1234567891;
+            musteri1.Ad = "Barış";
+            musteri1.SoyAd = "Osmanoğlu";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1234567892;
+            musteri2.Ad = "Baha";
+            musteri2.SoyAd = "Osmanoğlu";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1234567893;
+            musteri3.Ad = "Mustafa";
+            musteri3.SoyAd = "Osmanoğlu";
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.MusteriEkleme(musteri1);
+            musterimanager.MusteriEkleme(musteri2);
+            musterimanager.MusteriEkleme(musteri3);
+            
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Kayıtlı Müşterilerin Listesi ;");
+           
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.SoyAd);
+                
+            }
+
+            Console.WriteLine("---------------------------------------------------------------");
+
+            musterimanager.MusteriSil(musteri1);
+            musterimanager.MusteriSil(musteri2);
+            musterimanager.MusteriSil(musteri3);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
